d56be9ca39286611bc626176349f841caee19f99
Israel Cuevas <isracg@gmail.com>
2016-03-30 20:32:57 -0700

    A_GPS_can_be_created

---
+++ ReactDrone.Tests/GPS_Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ReactDrone.Tests
+{
+    public class GPS_Tests
+    {
+        [Fact]
+        public void A_GPS_can_be_created()
+        {
+            GPS.Create().Should().NotBeNull();
+        }
+    }
+
+    public class GPS
+    {
+        public static GPS Create()
+        {
+            return new GPS();
+        }
+    }
+}
