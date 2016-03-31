363de28a6909cc0103f4f213c0eb134ab463a4a3
Israel Cuevas <isracg@gmail.com>
2016-03-30 19:46:25 -0700

    A_drone_can_be_created

---
+++ ReactDrone.Tests/Drone_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ReactDrone.Tests
+{
+    public class Drone_Tests
+    {
+        [Fact]
+        public void A_drone_can_be_created()
+        {
+            Drone.Create().Should().NotBeNull();
+        }
+    }
+}
---
+++ ReactDrone/Drone.cs
@@ -0,0 +1,10 @@
+namespace ReactDrone
+{
+    public class Drone
+    {
+        public static Drone Create()
+        {
+            return new Drone();
+        }
+    }
+}
